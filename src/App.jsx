b30fb96f9061691bbd58753471e6239d9675ed96
pujaKundu <pujakundu797@gmail.com>
2023-11-01 11:01:51 +0600

    load and delete images

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {imagesData } from './constants/images'
+import { useState } from "react";
+import "./App.css";
+import { imagesData as initialImagesData } from "./constants/images";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imagesData, setImagesData] = useState(initialImagesData);
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  //toggles image selection
+  const handleImageSelect = (imageId) => {
+    //deselects image
+    if (selectedImages.includes(imageId)) {
+      setSelectedImages(selectedImages.filter((id) => id !== imageId));
+    } else {
+      //add image or select image
+      setSelectedImages([...selectedImages, imageId]);
+    }
+  };
+
+  const handleDelete = () => {
+    const updatedImages = imagesData.filter(
+      (image) => !selectedImages.includes(image?.id)
+    );
+    //update image list
+    setImagesData(updatedImages);
+    setSelectedImages([]);
+  };
 
   return (
     <>
-      {imagesData.map((image) => (
-        <div key={image.id}>
-          <img src={image.url} alt={`Image ${image.id}`} />
-        </div>
-      ))}
+      {selectedImages.length > 0 && (
+        <button
+          onClick={handleDelete}
+          className="absolute top-0 right-11 w-[150px] h-[50px] m-5 bg-red-600 hover:bg-red-700 text-white"
+        >
+          Delete files
+        </button>
+      )}
+      <div className="grid grid-cols-5 gap-4 mt-12">
+        {imagesData.map((image, index) => (
+          <div
+            key={image?.id}
+            className={index === 0 ? "col-span-2 row-span-2" : ""}
+            onClick={() => handleImageSelect(image.id)}
+          >
+            <div className="border">
+              <img
+                src={image?.url}
+                alt={`Image ${image?.id}`}
+                className="w-full h-full object-cover"
+              />
+              <input
+                type="checkbox"
+                className=" top-2 left-2"
+                onChange={() => handleImageSelect(image?.id)}
+                checked={selectedImages.includes(image?.id)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
