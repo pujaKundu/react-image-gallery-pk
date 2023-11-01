7ce5e0fa6fed5f4425f49bc909ef7b69a2ad25e2
pujaKundu <pujakundu797@gmail.com>
2023-11-01 20:25:24 +0600

    drag n drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { imagesData as initialImagesData } from "./constants/images";
-import icon from './assets/icons/checkbox.png'
+import icon from "./assets/icons/checkbox.png";
 
 function App() {
   const [imagesData, setImagesData] = useState(initialImagesData);
   const [selectedImages, setSelectedImages] = useState([]);
-  
+  const [featureImage, setFeatureImage] = useState(1);
+  const [draggedImage, setDraggedImage] = useState(null);
 
   //toggles image selection
   const handleImageSelect = (imageId) => {
@@ -28,7 +29,26 @@ function App() {
     setSelectedImages([]);
   };
 
-   
+  // drag and drop handlers
+  const handleDragStart = (event, image) => {
+    setDraggedImage(image);
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", event.target.parentNode);
+  };
+
+  const handleDragOver = (index) => {
+    const draggedOverImage = imagesData[index];
+    if (draggedImage === draggedOverImage) return;
+    let updatedImagesData = imagesData.filter(
+      (image) => image !== draggedImage
+    );
+    updatedImagesData.splice(index, 0, draggedImage);
+    setImagesData(updatedImagesData);
+  };
+
+  const handleDragEnd = () => {
+    setDraggedImage(null)
+  }
   return (
     <>
       {selectedImages.length > 0 ? (
@@ -60,8 +80,16 @@ function App() {
             key={image?.id}
             className={index === 0 ? "col-span-2 row-span-2" : ""}
             onClick={() => handleImageSelect(image.id)}
+            onDragOver={() => handleDragOver(index)}
           >
-            <div className="relative border rounded-lg ">
+            <div
+              draggable
+              onDragStart={(e) => handleDragStart(e, image)}
+              onDragEnd={handleDragEnd}
+              className={`relative border rounded-lg ${
+                image.id === draggedImage?.id ? "opacity-50" : "opacity-100"
+              }`}
+            >
               <input
                 type="checkbox"
                 className="absolute top-0 left-0 ml-8 mt-7 "
@@ -71,7 +99,7 @@ function App() {
               <img
                 src={image?.url}
                 alt={`Image ${image?.id}`}
-                className={`w-full h-full object-cover ${
+                className={`w-full h-full object-cover shadow-md  ${
                   selectedImages.includes(image?.id)
                     ? "opacity-50"
                     : "opacity-100"
@@ -79,6 +107,11 @@ function App() {
               />
               {/* for grayscale effect */}
               <div className="absolute top-0 left-0 w-full h-full bg-slate-900 opacity-0 hover:opacity-50 hover:rounded-lg transition duration-300 ease-in-out transform hover:grayscale"></div>
+              {image?.id === featureImage && (
+                <div className="absolute top-2 right-2 text-white text-sm bg-black bg-opacity-75 p-1 rounded">
+                  Featured
+                </div>
+              )}
             </div>
           </div>
         ))}
